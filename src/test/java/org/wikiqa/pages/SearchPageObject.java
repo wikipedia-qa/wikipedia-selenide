df8322ac91acf2e17f0a4e55444a71a831cf9580
Andrei Solntsev <andrei.solntsev@gmail.com>
2019-10-25 12:46:17 +0300

    typify the selectors (use By instead of String as much as possible)

--- src/test/java/org/wikiqa/pages/SearchPageObject.java
+++ src/test/java/org/wikiqa/pages/SearchPageObject.java
@@ -2,6 +2,7 @@ package org.wikiqa.pages;
 
 import com.codeborne.selenide.ElementsCollection;
 import org.openqa.selenium.By;
+import org.wikiqa.infra.Platform;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.disappear;
@@ -10,15 +11,14 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 abstract public class SearchPageObject extends MainPageObject {
-  static String
-      SEARCH_INIT_ELEMENT,
-      SEARCH_INPUT,
-      SEARCH_CANCEL_BUTTON,
-      SEARCH_RESULT_BY_SUBSTRING_TPL,
-      SEARCH_RESULT_ELEMENT,
-      SEARCH_EMPTY_RESULT_ELEMENT,
-      SEARCH_RESULTS_EMPTY_MESSAGE,
-      SEARCH_TITLE_AND_DESCRIPTION_TPL;
+  static By SEARCH_INIT_ELEMENT;
+  static By SEARCH_INPUT;
+  static By SEARCH_CANCEL_BUTTON;
+  static String SEARCH_RESULT_BY_SUBSTRING_TPL;
+  static By SEARCH_RESULT_ELEMENT;
+  static By SEARCH_EMPTY_RESULT_ELEMENT;
+  static By SEARCH_RESULTS_EMPTY_MESSAGE;
+  static String SEARCH_TITLE_AND_DESCRIPTION_TPL;
 
   private static String getResultSearchElement(String substring) {
     return SEARCH_RESULT_BY_SUBSTRING_TPL.replace("{SUBSTRING}", substring);
@@ -29,24 +29,26 @@ abstract public class SearchPageObject extends MainPageObject {
   }
 
   public void initSearchInput() {
-    $(By.xpath(SEARCH_INIT_ELEMENT)).shouldBe(visible.because("Search init element should be visible and clickable")).click();
-    $(By.xpath(SEARCH_INIT_ELEMENT)).should(disappear);
+    $(SEARCH_INIT_ELEMENT).shouldBe(visible.because("Search init element should be visible and clickable")).click();
+    if (Platform.getInstance().isIOS()) {
+      $(SEARCH_INIT_ELEMENT).should(disappear);
+    }
   }
 
   public void waitForCancelButtonToAppear() {
-    $(By.id(SEARCH_CANCEL_BUTTON)).shouldBe(visible);
+    $(SEARCH_CANCEL_BUTTON).shouldBe(visible);
   }
 
   public void waitForCancelButtonToDisappear() {
-    $(By.id(SEARCH_CANCEL_BUTTON)).shouldNotBe(visible);
+    $(SEARCH_CANCEL_BUTTON).shouldNotBe(visible);
   }
 
   public void clickCancelSearch() {
-    $(By.id(SEARCH_CANCEL_BUTTON)).shouldBe(visible).click();
+    $(SEARCH_CANCEL_BUTTON).shouldBe(visible).click();
   }
 
   public void typeSearchLine(String search_line) {
-    $(By.xpath(SEARCH_INPUT)).sendKeys(search_line);
+    $(SEARCH_INPUT).sendKeys(search_line);
   }
 
   public void waitForSearchResult(String substring) {
@@ -58,26 +60,19 @@ abstract public class SearchPageObject extends MainPageObject {
   }
 
   public ElementsCollection getFoundArticles() {
-    return $$(By.xpath(SEARCH_RESULT_ELEMENT));
-  }
-
-  /**
-   * @deprecated use getFoundArticles()
-   */
-  public int getAmountOfFoundArticles() {
-    return $$(By.xpath(SEARCH_RESULT_ELEMENT)).size();
+    return $$(SEARCH_RESULT_ELEMENT);
   }
 
   public void waitForEmptyResultsLabel() {
-    $(By.xpath(SEARCH_EMPTY_RESULT_ELEMENT)).shouldBe(visible);
+    $(SEARCH_EMPTY_RESULT_ELEMENT).shouldBe(visible);
   }
 
   public void assertThereIsNoResultOfSearch() {
-    $$(By.xpath(SEARCH_RESULT_ELEMENT)).shouldHave(size(0).because("We supposed not to find any results"));
+    $$(SEARCH_RESULT_ELEMENT).shouldHave(size(0).because("We supposed not to find any results"));
   }
 
   public void waitForSearchResultsToDisappear() {
-    $(By.id(SEARCH_RESULTS_EMPTY_MESSAGE)).shouldBe(visible);
+    $(SEARCH_RESULTS_EMPTY_MESSAGE).shouldBe(visible);
   }
 
   public void waitForElementByTitleAndDescription(String title, String description) {
